50f7f8ad8939ebdabd210b0718fe82872e7fc297
JustLishere <lisetepeelo@gmail.com>
2025-06-17 10:37:10 +0300

    kalk

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Types!");
+
+            //täisnumber
+            int bigNumber = 1234567890;
+            Console.WriteLine(bigNumber);
+
+            //komaga number ??
+            double komaNumber = 12.5;
+            Console.WriteLine(komaNumber);
+
+            //tähemärk
+            string stringVar = "¤¤¤)((y98y985(=)8";
+            Console.WriteLine(stringVar);
+
+            float floatVariable = 283.474f;
+            Console.WriteLine(floatVariable);
+
+            char cahrVariable = 'c';
+            Console.WriteLine(cahrVariable);
+
+            bool boolVariable = false;
+            Console.WriteLine(boolVariable);
+        }
+    }
+}
